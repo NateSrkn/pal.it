88e45d4bc8310ff04387585f0afbf08f56e18a96
Nathan Sorkin <NateSrkn@gmail.com>
2021-03-04 09:43:38 -0500

    Migrated server to it's own file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const pal_it = require("./helpers/helper");
 const app = express();
-const port = 3000;
 const paletteRoute = require("./routes/palette");
 app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 app.use("/api/palette", paletteRoute);
 
-app.listen(port, () => {
-  console.log(`Now listening on ${port}`);
-});
+module.exports = app;
